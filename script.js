12d31ec1179122ad03c8778e9cb57fdcb3e5c51a
unfelix <shevko.evheniy@gmail.com>
2020-05-20 20:49:08 +0300

    lights added

--- script.js
+++ script.js
@@ -40,10 +40,47 @@ function build(str) {
 function setFigure(row, col, figure) {
   if (!figure) return (board.rows[row].cells[col + 1].innerText = "");
   if ((row % 2 && col % 2) || (row % 2 == 0 && col % 2 == 0)) {
-    var cell = "black";
-  } else cell = "white";
-  if (cell != figure.color) {
+    var cell = "white";
+  } else cell = "black";
+  if (cell == figure.color) {
     var char = figures[figure.name].empty;
   } else char = figures[figure.name].filled;
   board.rows[row].cells[col + 1].innerText = char;
+}
+
+function fill(i, hue) {
+  i = ((i - 1) % 36) + 1;
+  const color = `hsl(${hue} 85% 90%)`;
+  const table = board.parentElement;
+  if (i <= 10) {
+    table.rows[0].cells[i - 1].style.background = color;
+  } else if (i < 19) {
+    board.rows[i - 11].cells[9].style.background = color;
+  } else if (i <= 28) {
+    table.rows[9].cells[28 - i].style.background = color;
+  } else {
+    board.rows[36 - i].cells[0].style.background = color;
+  }
+}
+
+for (let i = 1; i <= 1e3; i++) {
+  setTimeout(fill, 20 * i, i, i);
+}
+
+function write(i, letter) {
+  i = ((i - 1) % 36) + 1;
+  const table = board.parentElement;
+  if (i <= 10) {
+    table.rows[0].cells[i - 1].innerText = letter;
+  } else if (i < 19) {
+    board.rows[i - 11].cells[9].innerText = letter;
+  } else if (i <= 28) {
+    table.rows[9].cells[28 - i].innerText = letter;
+  } else {
+    board.rows[36 - i].cells[0].innerText = letter;
+  }
+}
+
+for (let i = 1; i < 1e3; i++) {
+  setTimeout(write, 500 * i, i, i);
 }
